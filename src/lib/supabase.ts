1d8b445726c1fb51968623edf223793afc4a5536
Isaac Dyor <isaac@dyor.com>
2024-06-09 12:14:11 -0700

    setting up auth

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://ycsddwavhwlyursordup.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inljc2Rkd2F2aHdseXVyc29yZHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4ODQ1NTQsImV4cCI6MjAzMzQ2MDU1NH0.yV9j0E0828-4f6jhIe5mYW6Gzfd11nYGevPjl1HyvHw";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
